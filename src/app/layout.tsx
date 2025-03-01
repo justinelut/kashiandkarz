9cbd48310bbd9b5e075787ceab105ac2cb70e7c3
David Muriithi <88942514+BarriosXJavier@users.noreply.github.com>
2025-03-01 11:48:05 +0000

    Remove Navbar component and add "use client" directive to multiple components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body
         className={`antialiased ${montserrat.variable}`}
       >
-        <Navbar />
+       
         {children}
         <Footer />
       </body>
--- src/components/hero-with-nav.tsx
+++ src/components/hero-with-nav.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useEffect, useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { motion, useAnimation, AnimatePresence } from "framer-motion";
